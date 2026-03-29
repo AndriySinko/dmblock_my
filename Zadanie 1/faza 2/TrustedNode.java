3afe6a25b3facdc6876fb24ee7a6a6f4337bd182
AndriiSynko <sinkoandriy7@gmail.com>
2026-03-29 20:29:05 +0200

    phase 2: implemented constructor, init folowees and transaction pool

--- Zadanie 1/faza 2/TrustedNode.java
+++ Zadanie 1/faza 2/TrustedNode.java
@@ -1,27 +1,39 @@
 // Meno študenta: Andrii Synko
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 /* TrustedNode označuje uzol, ktorý dodržuje pravidlá (nie je byzantský) */
 public class TrustedNode implements Node {
 
+    private double p_graph;
+    private double p_byzantine;
+    private double p_txDistribution;
+    private int numRounds;
+
+    private boolean[] followees;
+    private Set<Transaction> transactionsPool;
+
     public TrustedNode(double p_graph, double p_byzantine, double p_txDistribution, int numRounds) {
-        // IMPLEMENTOVAŤ
+        this.p_graph = p_graph;
+        this.p_byzantine = p_byzantine;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
+
     }
 
     public void followeesSet(boolean[] followees) {
-        // IMPLEMENTOVAŤ
+        this.followees = followees; // get your folowwes
     }
 
     public void pendingTransactionSet(Set<Transaction> pendingTransactions) {
-        // IMPLEMENTOVAŤ
+        transactionsPool = new HashSet<>(pendingTransactions); // init pool
     }
 
     public Set<Transaction> followersSend() {
-        // IMPLEMENTOVAŤ
+        return transactionsPool; // return transactions that node believes in
     }
 
     public void followeesReceive(ArrayList<Integer[]> candidates) {
-        // IMPLEMENTOVAŤ
     }
 }
