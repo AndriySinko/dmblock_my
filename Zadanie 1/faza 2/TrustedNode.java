e4c1c8624b0026f2a0863c3d4223e5ae84585e51
AndriiSynko <sinkoandriy7@gmail.com>
2026-03-31 21:53:54 +0200

    phase 2: receive logic (append only)

--- Zadanie 1/faza 2/TrustedNode.java
+++ Zadanie 1/faza 2/TrustedNode.java
@@ -1,25 +1,17 @@
 // Meno študenta: Andrii Synko
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 /* TrustedNode označuje uzol, ktorý dodržuje pravidlá (nie je byzantský) */
 public class TrustedNode implements Node {
 
-    private double p_graph;
-    private double p_byzantine;
-    private double p_txDistribution;
-    private int numRounds;
-
     private boolean[] followees;
     private Set<Transaction> transactionsPool;
 
     public TrustedNode(double p_graph, double p_byzantine, double p_txDistribution, int numRounds) {
-        this.p_graph = p_graph;
-        this.p_byzantine = p_byzantine;
-        this.p_txDistribution = p_txDistribution;
-        this.numRounds = numRounds;
-
+        this.transactionsPool = new HashSet<>();
     }
 
     public void followeesSet(boolean[] followees) {
@@ -33,7 +25,23 @@ public class TrustedNode implements Node {
     public Set<Transaction> followersSend() {
         return transactionsPool; // return transactions that node believes in
     }
-
     public void followeesReceive(ArrayList<Integer[]> candidates) {
+        HashMap<Integer, Set<Integer>> txToSenders = new HashMap<>(); // stores senders of the txId
+        for (Integer[] candidate : candidates) {
+            int txId = candidate[0];
+            int sender = candidate[1];
+            if (!txToSenders.containsKey(txId)) {
+                txToSenders.put(txId, new HashSet<>()); // add tx if absent
+            }
+            txToSenders.get(txId).add(sender); // put there sender
+        }
+
+        int threshold = 1; // one cuz byzantines are silent, otherwise calculate based on followees
+
+        for (int txId : txToSenders.keySet()) {
+            if (txToSenders.get(txId).size() >= threshold) { // if transaction received count > threshold, add to pool
+                transactionsPool.add(new Transaction(txId));
+            }
+        }
     }
 }
